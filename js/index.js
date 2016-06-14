ff5540b82e8f2ab10de364b4f77a6dda25d61714
george robbins <george robbins>
2016-06-14 16:17:39 -0400

    added die functionality

--- js/index.js
+++ js/index.js
@@ -6,12 +6,54 @@ function randomSide(){
 	return Math.random()*(max-min)+min;
 }
 
+function getSideImgUrl(side){
+	var diceImg;
+	switch(side){
+		case 1:
+			diceImg = 'img/one.gif';
+			break;
+		case 2:
+			diceImg = 'img/two.gif';
+			break
+		case 3:
+			diceImg = 'img/three.jpg';
+			break;
+		case 4:
+			diceImg = 'img/four.png';
+			break				
+		case 5:
+			diceImg = 'img/five.png';
+			break;
+		case 6:
+			diceImg = 'img/six.jpg';
+			break				
+	}
+
+	return diceImg;
+}
 
 (function($){
 	$("#die1").on('click', function(evt){
 		$("die1").remove("side");
-		var side = randomSide();
-		$("die1").append("<p id=side>"+side+"</p>");
+		var side = Math.ceil( randomSide() );
+		var diceImg = getSideImgUrl(side);
+
+		$("#die1 img").remove();
+		$("#die1").prepend("<img class='side' src='" +diceImg+ "'/>");
+	});
+
+	$("#die2").on('click', function(evt){
+		$("die2").remove("side");
+		var side = Math.ceil( randomSide() );
+		var diceImg = getSideImgUrl(side);
+
+		$("#die2 img").remove();
+		$("#die2").prepend("<img class='side' src='" +diceImg+ "'/>");
+	});	
+
+	$('#roll').on('click', function(evt){
+		$("#die1").click();
+		$("#die2").click();
 	});
 })(jQuery);
 
